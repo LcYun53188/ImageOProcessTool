8ba984f28c7629842dd3ab6e4dac2a22fb389751
LcYun <74088354+LcYun53188@users.noreply.github.com>
2025-03-22 13:40:09 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace ImageProcessingTool
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnSaveNext;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TrackBar trackBarBrushSize;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnSaveNext = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.trackBarBrushSize = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBrushSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(12, 12);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "选择文件夹";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(776, 397);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom; // 设置为Zoom模式
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox_MouseUp);
+            // 
+            // btnSaveNext
+            // 
+            this.btnSaveNext.Location = new System.Drawing.Point(93, 12);
+            this.btnSaveNext.Name = "btnSaveNext";
+            this.btnSaveNext.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveNext.TabIndex = 2;
+            this.btnSaveNext.Text = "保存并下一张";
+            this.btnSaveNext.UseVisualStyleBackColor = true;
+            this.btnSaveNext.Click += new System.EventHandler(this.btnSaveNext_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(174, 12);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(75, 23);
+            this.btnUndo.TabIndex = 3;
+            this.btnUndo.Text = "撤销上一步";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(255, 12);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "撤销所有操作";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // trackBarBrushSize
+            // 
+            this.trackBarBrushSize.Location = new System.Drawing.Point(713, 12);
+            this.trackBarBrushSize.Maximum = 100;
+            this.trackBarBrushSize.Minimum = 1;
+            this.trackBarBrushSize.Name = "trackBarBrushSize";
+            this.trackBarBrushSize.Size = new System.Drawing.Size(75, 45);
+            this.trackBarBrushSize.TabIndex = 5;
+            this.trackBarBrushSize.Value = 15;
+            this.trackBarBrushSize.Scroll += new System.EventHandler(this.trackBarBrushSize_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.trackBarBrushSize);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnSaveNext);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Name = "Form1";
+            this.Text = "图片处理工具";
+            this.WindowState = FormWindowState.Maximized; // 设置窗体启动时全屏
+            this.FormBorderStyle = FormBorderStyle.None; // 设置无边框
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBrushSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 设置全屏
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageProcessingTool
+{
+    public partial class Form1 : Form
+    {
+        private List<string> imageFiles;
+        private int currentImageIndex;
+        private Bitmap currentImage;
+        private List<Bitmap> undoStack;
+        private Graphics graphics;
+        private bool isCtrlPressed;
+        private bool isDrawing;
+        private bool isErasing;
+        private Point lastPoint;
+        private int brushSize;
+        private BrushType currentBrush;
+        private float zoomFactor = 1.0f;
+        private PointF imageOffset = PointF.Empty;
+
+        public Form1()
+        {
+            InitializeComponent();
+            imageFiles = new List<string>();
+            undoStack = new List<Bitmap>();
+            currentImageIndex = -1;
+            isCtrlPressed = false;
+            isDrawing = false;
+            isErasing = false;
+            lastPoint = Point.Empty;
+            brushSize = 15;
+            currentBrush = BrushType.Circle;
+            pictureBox.MouseDown += PictureBox_MouseDown;
+            pictureBox.MouseMove += PictureBox_MouseMove;
+            pictureBox.MouseUp += PictureBox_MouseUp;
+            KeyDown += Form1_KeyDown;
+            KeyUp += Form1_KeyUp;
+            pictureBox.Paint += PictureBox_Paint;
+            pictureBox.MouseWheel += PictureBox_MouseWheel;
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    imageFiles.Clear();
+                    imageFiles.AddRange(Directory.GetFiles(dialog.SelectedPath, "*.jpg"));
+                    imageFiles.AddRange(Directory.GetFiles(dialog.SelectedPath, "*.png"));
+                    currentImageIndex = -1;
+                    LoadNextImage();
+                }
+            }
+        }
+
+        private void LoadNextImage()
+        {
+            if (imageFiles.Count == 0)
+            {
+                MessageBox.Show("没有找到图像文件。");
+                return;
+            }
+
+            currentImageIndex++;
+            if (currentImageIndex >= imageFiles.Count)
+            {
+                currentImageIndex = 0;
+            }
+
+            currentImage?.Dispose();
+            currentImage = new Bitmap(imageFiles[currentImageIndex]);
+            pictureBox.Image = currentImage;
+
+            // 自动将图片缩放到合适的大小
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+            AdjustImageToFit();
+            graphics = Graphics.FromImage(currentImage);
+            undoStack.Clear();
+        }
+
+        private void AdjustImageToFit()
+        {
+            if (currentImage != null)
+            {
+                float aspectRatio = (float)currentImage.Width / currentImage.Height;
+                if (this.ClientSize.Width / (float)this.ClientSize.Height > aspectRatio)
+                {
+                    pictureBox.Width = (int)(this.ClientSize.Height * aspectRatio);
+                    pictureBox.Height = this.ClientSize.Height;
+                }
+                else
+                {
+                    pictureBox.Width = this.ClientSize.Width;
+                    pictureBox.Height = (int)(this.ClientSize.Width / aspectRatio);
+                }
+                pictureBox.Left = (this.ClientSize.Width - pictureBox.Width) / 2;
+                pictureBox.Top = (this.ClientSize.Height - pictureBox.Height) / 2;
+            }
+        }
+
+        private void btnSaveNext_Click(object sender, EventArgs e)
+        {
+            if (currentImage != null)
+            {
+                string savePath = Path.Combine(Path.GetDirectoryName(imageFiles[currentImageIndex]), "Processed", Path.GetFileName(imageFiles[currentImageIndex]));
+                Directory.CreateDirectory(Path.GetDirectoryName(savePath));
+                currentImage.Save(savePath);
+                currentImageIndex++;
+                LoadNextImage();
+            }
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (undoStack.Count > 0)
+            {
+                currentImage = new Bitmap(undoStack[undoStack.Count - 1]);
+                undoStack.RemoveAt(undoStack.Count - 1);
+                pictureBox.Image = currentImage;
+                graphics = Graphics.FromImage(currentImage);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            LoadNextImage();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.ControlKey:
+                    isCtrlPressed = true;
+                    break;
+                case Keys.Space:
+                    btnSaveNext.PerformClick();
+                    break;
+                case Keys.Q:
+                    currentBrush = BrushType.Circle;
+                    break;
+                case Keys.W:
+                    currentBrush = BrushType.Ring;
+                    break;
+                case Keys.Z:
+                    btnUndo.PerformClick();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                isCtrlPressed = false;
+            }
+        }
+
+        private void PictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = true;
+                PointF transformedPoint = TransformToImageCoordinates(e.Location);
+                lastPoint = Point.Round(transformedPoint); // 使用 Point.Round 将 PointF 转换为 Point
+                SaveUndoState();
+            }
+        }
+
+        private void PictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                PointF transformedPoint = TransformToImageCoordinates(e.Location);
+                Draw(Point.Round(transformedPoint)); // 使用 Point.Round 将 PointF 转换为 Point
+                lastPoint = Point.Round(transformedPoint);
+            }
+        }
+
+        private void PictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                isDrawing = false;
+            }
+        }
+
+        private void trackBarBrushSize_Scroll(object sender, EventArgs e)
+        {
+            brushSize = trackBarBrushSize.Value;
+        }
+
+        private void Draw(Point location)
+        {
+            using (Pen pen = new Pen(Color.Red, brushSize))
+            {
+                switch (currentBrush)
+                {
+                    case BrushType.Circle:
+                        graphics.FillEllipse(pen.Brush, location.X - brushSize / 2, location.Y - brushSize / 2, brushSize, brushSize);
+                        break;
+                    case BrushType.Ring:
+                        graphics.DrawEllipse(pen, location.X - brushSize / 2, location.Y - brushSize / 2, brushSize, brushSize);
+                        break;
+                    default:
+                        break;
+                }
+                pictureBox.Invalidate();
+            }
+        }
+
+        private void SaveUndoState()
+        {
+            if (currentImage != null)
+            {
+                undoStack.Add(new Bitmap(currentImage));
+                if (undoStack.Count > 10)
+                {
+                    undoStack.RemoveAt(0);
+                }
+            }
+        }
+
+        private PointF TransformToImageCoordinates(Point point)
+        {
+            if (currentImage == null)
+                return PointF.Empty;
+
+            float x = (point.X - imageOffset.X) / zoomFactor;
+            float y = (point.Y - imageOffset.Y) / zoomFactor;
+
+            return new PointF(x, y);
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (currentImage != null)
+            {
+                e.Graphics.DrawImage(currentImage, imageOffset.X, imageOffset.Y, currentImage.Width * zoomFactor, currentImage.Height * zoomFactor);
+            }
+        }
+
+        private void PictureBox_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (isCtrlPressed)
+            {
+                float zoomDelta = 0.1f; // 缩放步长
+                if (e.Delta > 0)
+                {
+                    zoomFactor += zoomDelta;
+                }
+                else
+                {
+                    zoomFactor -= zoomDelta;
+                    if (zoomFactor < 0.1f) // 防止缩放比例过小
+                        zoomFactor = 0.1f;
+                }
+
+                // 更新偏移量，保持鼠标位置不变
+                Point mousePos = new Point(e.X, e.Y);
+                PointF oldMousePosInImage = TransformToImageCoordinates(mousePos);
+                imageOffset.X = e.X - oldMousePosInImage.X * zoomFactor;
+                imageOffset.Y = e.Y - oldMousePosInImage.Y * zoomFactor;
+
+                pictureBox.Invalidate(); // 重新绘制图片
+            }
+        }
+    }
+
+    public enum BrushType
+    {
+        Circle,
+        Ring
+    }
+}
